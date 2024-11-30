01b41526d6eb585037ca1b7098d39c2e85662969
Sanjana kesharwani <159414407+Sanjana8kesharwani@users.noreply.github.com>
2024-11-30 12:14:17 +0530

    Delete portfilo directory

--- portfilo/myscripts.js
+++
@@ -1,8 +0,0 @@
-let crser = document.querySelector("#curser");
-let blr=document.querySelector("#curser_blur");
-document.addEventListener("mousemove", function (dets) {
-    crser.style.left = dets.x + "px";
-    crser.style.top = dets.y + "px";
-    blr.style.left = dets.x - 250 + "px";
-    blr.style.top = dets.y - 250 + "px";
-})
